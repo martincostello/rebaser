e5c3820afe259011d57231668180249d6b8c511d
martincostello <martin@martincostello.com>
2023-07-30 12:25:30 +0100

    Add GitHub Actions reporter
    Add the GitHub Actions reporter for tests.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: [ 'js', 'ts' ],
   prettierPath: null,
+  reporters: [
+    ['github-actions', {silent: false}], 'summary'
+  ],
   testEnvironment: 'node',
   testMatch: [ '**/*.test.ts' ],
   transform: {
